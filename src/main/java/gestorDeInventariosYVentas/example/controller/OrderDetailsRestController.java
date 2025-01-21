69751d3500dc3e795d34a6ae12903f89ce4773a9
Jairo Alejandro <jairo47peralta@gmail.com>
2025-01-20 21:09:08 -0500

    Enero 20, Corrección en la ruta de los controladores

--- src/main/java/gestorDeInventariosYVentas/example/controller/OrderDetailsRestController.java
+++ src/main/java/gestorDeInventariosYVentas/example/controller/OrderDetailsRestController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/orders-details")
+@RequestMapping("/api/v1/orders-details")
 public class OrderDetailsRestController {
 
     private final OrderDetailsService orderDetailsService;
